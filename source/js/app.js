6c4be6b0544c9a48f1a51cc9a1c4d16daf689b8f
SaraNiknamHesar <saraniknaam76@gmail.com>
2024-10-11 10:40:06 +0330

    light_dark_mode

--- source/js/app.js
+++ source/js/app.js
@@ -2,11 +2,8 @@
 let theme = localStorage.getItem("theme");
 let switchElement = document.querySelector(".switch");
 let body = document.getElementsByTagName("body")[0];
-
-// Add click event listener to toggle theme
 switchElement.addEventListener("click", () => {
   if (localStorage.getItem("theme") === "light") {
-    // body.classList.remove("light");
     body.classList.add("dark");
     localStorage.setItem("theme", "dark");
   } else {
